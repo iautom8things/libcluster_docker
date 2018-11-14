7362b6f4de04a791a11dea697638b0bf535c0ebd
Manuel Zubieta <mazubieta@gmail.com>
2018-11-13 20:20:54 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClusterDocker.MixProject do
   def project do
     [
       app: :libcluster_docker,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       name: "libcluster_docker",
       start_permanent: Mix.env() == :prod,
